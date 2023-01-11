e563559b10a9b2f4577ff88aaaabc0ae328884fa
Willyam Ramos <ramoswillyam1@gmail.com>
2023-01-10 23:11:34 -0800

    Attempting to solve issue of localstorage avoiding first category-container and passing the info of the first localstorage item into the initial category container

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 const addButton = document.querySelector("#addButton");
 const subtractButton = document.querySelector("#subtractButton");
 const formContainer = document.querySelector("#formContainer");
-const allCategoryContainers = document.querySelectorAll(".category-container")
 let localCategoryData = JSON.parse(window.localStorage.getItem('Categories'));
+
+function updateCategories() {
+    let allCategoryContainers = document.querySelectorAll(".category-container");
+    let categoryData = []
+
+    for (let i = 0; i < allCategoryContainers.length; i++) {
+        let obj = {};
+        [...allCategoryContainers[i].children].map((item) => [...item.children].map((val) => {
+            obj[val.id] = val.value;
+        }))
+        categoryData.push(obj);
+    }
+    window.localStorage.setItem('Categories', JSON.stringify(categoryData));
+}
 
 function getCategories() {
     if (localCategoryData) {
         localCategoryData.map((category) => {
-            console.log(category);
+            createStoredCategory(category);
         })
     } else {
-        let categoryData = []
-
-        for (let i = 0; i < allCategoryContainers.length; i++) {
-            let obj = {};
-            [...allCategoryContainers[i].children].map((item) => [...item.children].map((val) => {
-                obj[val.id] = val.value;
-            }))
-            categoryData.push(obj);
-        }
-        window.localStorage.setItem('Categories', JSON.stringify(categoryData));
+        updateCategories();
     }
 }
 
-window.addEventListener('onload', getCategories());
 
-
-const createCategory = () => {
+function createCategory() {
     const newCategoryDiv = document.createElement('div');
     newCategoryDiv.className = 'category-container';
     for (let i = 0; i < 3; i++) {
@@ -51,6 +53,40 @@ const createCategory = () => {
         newCategoryDiv.append(category)
     }
     formContainer.append(newCategoryDiv);
+    updateCategories();
 };
+
+function createStoredCategory(storedCategory) {
+    const newCategoryDiv = document.createElement('div');
+    newCategoryDiv.className = 'category-container';
+    for (let i = 0; i < 3; i++) {
+        const textInput = document.createElement('input');
+        const category = document.createElement('div');
+        category.classList.add('category');
+        textInput.type = 'text';
+        if (i === 0) {
+            textInput.value = storedCategory['name'];
+            textInput.id = 'name';
+            textInput.placeholder = 'Enter category name'
+        }
+        else if (i === 1) {
+            textInput.value = storedCategory['weight'];
+            textInput.id = 'weight';
+            textInput.placeholder = '%'
+        }
+        else if (i === 2) {
+            textInput.value = storedCategory['grade'];
+            textInput.id = 'grade';
+            textInput.placeholder = '%'
+        }
+
+        category.append(textInput);
+        newCategoryDiv.append(category)
+    }
+    formContainer.append(newCategoryDiv);
+};
+
+
+window.addEventListener('onload', getCategories());
 
 addButton.addEventListener('click', createCategory)
