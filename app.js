d46b36ac4f0a5f2f62da5209fc4da1643453e7ed
Willyam Ramos <ramoswillyam1@gmail.com>
2023-01-11 20:12:13 -0800

    Added functionality to calculate average grade, tweaked localstorage to save data properly, and created dom elements to show grade. Styled page minimally, but will add more soon

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const addButton = document.querySelector("#addButton");
 const subtractButton = document.querySelector("#subtractButton");
+const submitButton = document.querySelector("#submitBtn");
 const formContainer = document.querySelector("#formContainer");
+const gradeContainer = document.querySelector(".grade-container");
+const listedCategories = document.querySelector(".category-list");
+const estimatedGradeDiv = document.querySelector(".grade-estimate");
 let localCategoryData = JSON.parse(window.localStorage.getItem('Categories'));
 
 function updateCategories() {
@@ -15,6 +19,7 @@ function updateCategories() {
         categoryData.push(obj);
     }
     window.localStorage.setItem('Categories', JSON.stringify(categoryData));
+    localCategoryData = JSON.parse(window.localStorage.getItem('Categories'));
 }
 
 function getCategories() {
@@ -102,8 +107,65 @@ function createStoredCategory(storedCategory) {
     formContainer.append(newCategoryDiv);
 };
 
+function calculateGrade() {
+    updateCategories();
+    document.querySelectorAll('.new').forEach(e => e.remove());
+
+
+    let totalWeight = [];
+    let totalPercentageOfWeight = 0;
+    localCategoryData.map((category) => {
+        totalWeight.push(category.weight.length ? (Number(category.weight / 100) * category.grade).toFixed(1) : 0);
+        totalPercentageOfWeight += Number(category.weight);
+    })
+
+    totalPercentageOfWeight = totalPercentageOfWeight / 100;
+
+    let calculatedGrade = ((totalWeight.reduce((a, b) => a + Number(b), 0).toFixed(2) / totalPercentageOfWeight).toFixed(2));
+
+    for (let i = 0; i < localCategoryData.length; i++) {
+        if (localCategoryData[i]['name'] === '' || localCategoryData[i]['weight'] === '' || localCategoryData[i]['grade'] === '') {
+            alert('Please provide fill out all input fields');
+            return;
+        }
+        const newCategoryListItem = document.createElement('li');
+        newCategoryListItem.className = 'grade-category';
+        newCategoryListItem.className += ' new'
+
+        const categoryName = document.createElement('p');
+        categoryName.textContent = localCategoryData[i]['name'];
+        categoryName.className = 'category-info';
+        categoryName.className += ' new'
+
+        const categoryWeight = document.createElement('p');
+        categoryWeight.textContent = localCategoryData[i]['weight'] + '%';
+        categoryWeight.className = 'category-info';
+        categoryWeight.className += ' new';
+
+        const categoryGrade = document.createElement('p');
+        categoryGrade.textContent = localCategoryData[i]['grade'] + '%';
+        categoryGrade.className = 'category-info';
+        categoryGrade.className += ' new'
+
+        newCategoryListItem.appendChild(categoryName);
+        newCategoryListItem.appendChild(categoryWeight);
+        newCategoryListItem.appendChild(categoryGrade);
+
+        listedCategories.appendChild(newCategoryListItem);
+    }
+
+    const gradeTitle = document.createElement('h1');
+    gradeTitle.className = 'new'
+    gradeTitle.textContent = `Average Grade: ${calculatedGrade}%`;
+
+    estimatedGradeDiv.appendChild(gradeTitle);
+
+    gradeContainer.style.display = 'flex';
+
+};
+
 
 window.addEventListener('onload', getCategories());
-
-addButton.addEventListener('click', createCategory)
-subtractButton.addEventListener('click', deleteCategory)
+submitButton.addEventListener('click', calculateGrade);
+addButton.addEventListener('click', createCategory);
+subtractButton.addEventListener('click', deleteCategory);
